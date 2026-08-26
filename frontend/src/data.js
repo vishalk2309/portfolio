5b3c0560d1b30332fe6e86359afefbda7250c033
Vishal Kushwaha <kushwahavishal296@gmail.com>
2026-08-26 16:36:28 +0530

    social site whatsapp fixed

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -66,7 +66,7 @@ export const socials = [
   },
   {
     Icon: FaWhatsapp,
-    href: "https://whatsapp.com/channel/0029VbDjWfgHQbS6XEdBjf0H",
+    href: "https://whatsapp.com/channel/0029VbDjWfgHQbS6XRdBjf0H",
     label: "WhatsApp",
   },
   {
